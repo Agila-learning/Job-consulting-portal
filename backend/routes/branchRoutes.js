2faa9971751deb055c37db3c2e5bee5506716c38
Agila-learning <agilaagi0922@gmail.com>
2026-04-09 10:35:13 +0530

    fix render error

--- backend/routes/branchRoutes.js
+++ backend/routes/branchRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getBranches, createBranch } = require('../controllers/branchController');
-const { protect, authorize } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/authMiddleware');
 
 router.get('/', protect, getBranches);
 router.post('/', protect, authorize('admin'), createBranch);
